7f2c1a42dad93bfa4a5c68de54ff245e780659c8
Dmitry Prokhorov <dipspb@gmail.com>
2019-10-30 13:26:07 +0300

    SSL certificates renew script hardcoded values removed

--- examples/letsencrypt/renew.sh
+++ examples/letsencrypt/renew.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-./letsencrypt.sh --domain brain.5coders.com --email dipspb@gmail.com --volume jupyterhub-secrets
+DOMAIN=$1
+EMAIL=$2
+
+./letsencrypt.sh --domain ${DOMAIN} --email ${EMAIL} --volume jupyterhub-secrets
 
